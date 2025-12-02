aae80cdb2baba6de1bdb768ef5d203314c57dc28
Josh Emerson <jemerson@vontweb.com>
2025-12-02 10:35:36 -0500

    practicum completed - sending data to custom object

--- index.js
+++ index.js
@@ -23,84 +23,57 @@ app.get('/', async (req, res) => {
         const resp = await axios.get(practicum_reviewers, { headers });
         const data = resp.data.results;
         res.render('homepage', { title: 'Practicum reviewers homepage', data });
-        // res.json(resp.data.results)
     } catch (error) {
         console.error(error);
     }
 });
-
-app.get('/update-cobj', async (req, res) => {
-    // const practicum_reviewers = 'https://api.hubspot.com/crm/v3/objects/2-53863368';
-    const headers = {
-        // Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        // const resp = await axios.get(practicum_reviewers, { headers });
-        // const data = resp.data.results;
-        res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' });
-        // res.json(resp.data.results)
-    } catch (error) {
-        console.error(error);
-    }
-});
-
-
 
 
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
+app.get('/update-cobj', async (req, res) => {
     const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
     }
     try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
+        res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' });
     } catch (error) {
         console.error(error);
     }
 });
 
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
+// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
+// * Code for Route 3 goes here
+app.post('/update-cobj', async (req, res) => {
+    const reviewerData = {
+        properties: {
+          'name': req.body.name,
+          'favorite_custom_object': req.body.favorite_custom_object,
+          'practicum_experience': req.body.practicum_experience,
+        }
+    };
+
+    const updateReviewers = 'https://api.hubspot.com/crm/v3/objects/2-53863368';
     const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        Authorization:`Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
     };
 
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
+    try {
+        await axios.post(updateReviewers, reviewerData, { headers } );
+        res.redirect('/');
     } catch(err) {
-        console.error(err);
+        console.error('HubSpot API Error:', err.response ? err.response.data : err.message);
+        res.status(err.response ? err.response.status : 500).send({
+            message: 'HubSpot request failed.',
+            details: err.response ? err.response.data : 'Internal server error.'
+        });
     }
 
 });
-*/
-
 
 // * Localhost
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
